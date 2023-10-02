f6eaef2d9ad44f1d6dc35b289e524bb6c5d27560
chris <crees@uncc.edu>
2023-10-02 12:58:06 -0400

    The culmination

--- p2.jsx
+++ p2.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import States from './components/states/States';
+import Header from "./components/header/Header";
 
 ReactDOM.render(
-  <States />,
-  document.getElementById('reactapp'),
+    <Header/>,
+    document.getElementById('reactappHeader')
+);
+
+ReactDOM.render(
+    <States />,
+    document.getElementById('reactapp'),
 );
